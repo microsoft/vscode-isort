0bf1b0b47fa76a4d6d19c5d2069d75e415c363aa
Karthik Nadig <kanadig@microsoft.com>
2022-11-04 17:26:00 -0700

    Fix tests.

--- src/test/ts_tests/tests/common/settings.unit.test.ts
+++ src/test/ts_tests/tests/common/settings.unit.test.ts
@@ -3,6 +3,7 @@
 
 import * as path from 'path';
 import * as sinon from 'sinon';
+import * as TypeMoq from 'typemoq';
 import * as vscodeapi from '../../../../common/vscodeapi';
 import * as python from '../../../../common/python';
 import { assert } from 'chai';
@@ -10,51 +11,12 @@ import { ConfigurationTarget, Uri, WorkspaceConfiguration, WorkspaceFolder } fro
 import { EXTENSION_ROOT_DIR } from '../../../../common/constants';
 import { getWorkspaceSettings, ISettings } from '../../../../common/settings';
 
-class MockConfig implements WorkspaceConfiguration {
-    readonly [key: string]: any;
-    get<T>(section: string): T | undefined;
-    get<T>(section: string, defaultValue: T): T;
-    get(section: unknown, defaultValue?: unknown): unknown | undefined {
-        return undefined;
-    }
-    has(section: string): boolean {
-        throw new Error('Method not implemented.');
-    }
-    inspect<T>(section: string):
-        | {
-              key: string;
-              defaultValue?:
-                  | T
-                  // Licensed under the MIT License. // Licensed under the MIT License.
-                  | undefined;
-              globalValue?: T | undefined;
-              workspaceValue?: T | undefined;
-              workspaceFolderValue?: T | undefined;
-              defaultLanguageValue?: T | undefined;
-              globalLanguageValue?: T | undefined;
-              workspaceLanguageValue?: T | undefined;
-              workspaceFolderLanguageValue?: T | undefined;
-              languageIds?: string[] | undefined;
-          }
-        | undefined {
-        throw new Error('Method not implemented.');
-    }
-    update(
-        section: string,
-        value: any,
-        configurationTarget?: boolean | ConfigurationTarget | null | undefined,
-        overrideInLanguage?: boolean | undefined,
-    ): Thenable<void> {
-        return Promise.resolve();
-    }
-}
-
 suite('Settings Tests', () => {
     suite('getWorkspaceSettings tests', () => {
         let getConfigurationStub: sinon.SinonStub;
         let getInterpreterDetailsStub: sinon.SinonStub;
-        let config: WorkspaceConfiguration;
-        let configMock: sinon.SinonMock;
+        let configMock: TypeMoq.IMock<WorkspaceConfiguration>;
+        let pythonConfigMock: TypeMoq.IMock<WorkspaceConfiguration>;
         let workspace1: WorkspaceFolder = {
             uri: Uri.file(path.join(EXTENSION_ROOT_DIR, 'src', 'test', 'testWorkspace', 'workspace1')),
             name: 'workspace1',
@@ -64,9 +26,14 @@ suite('Settings Tests', () => {
         setup(() => {
             getConfigurationStub = sinon.stub(vscodeapi, 'getConfiguration');
             getInterpreterDetailsStub = sinon.stub(python, 'getInterpreterDetails');
-            config = new MockConfig();
-            configMock = sinon.mock(config);
-            getConfigurationStub.returns(config);
+            configMock = TypeMoq.Mock.ofType<WorkspaceConfiguration>();
+            pythonConfigMock = TypeMoq.Mock.ofType<WorkspaceConfiguration>();
+            getConfigurationStub.callsFake((namespace: string, uri: Uri) => {
+                if (namespace.startsWith('isort')) {
+                    return configMock.object;
+                }
+                return pythonConfigMock.object;
+            });
         });
 
         teardown(() => {
@@ -75,6 +42,44 @@ suite('Settings Tests', () => {
 
         test('Default Settings test', async () => {
             getInterpreterDetailsStub.resolves({ path: undefined });
+            configMock
+                .setup((c) => c.get('args', []))
+                .returns(() => [])
+                .verifiable(TypeMoq.Times.atLeastOnce());
+            configMock
+                .setup((c) => c.get('path', []))
+                .returns(() => [])
+                .verifiable(TypeMoq.Times.atLeastOnce());
+            configMock
+                .setup((c) => c.get('check', false))
+                .returns(() => false)
+                .verifiable(TypeMoq.Times.atLeastOnce());
+            configMock
+                .setup((c) => c.get('logLevel', 'error'))
+                .returns(() => 'error')
+                .verifiable(TypeMoq.Times.atLeastOnce());
+            configMock
+                .setup((c) => c.get('severity', {}))
+                .returns(() => ({}))
+                .verifiable(TypeMoq.Times.atLeastOnce());
+            configMock
+                .setup((c) => c.get('importStrategy', 'useBundled'))
+                .returns(() => 'useBundled')
+                .verifiable(TypeMoq.Times.atLeastOnce());
+            configMock
+                .setup((c) => c.get('showNotifications', 'off'))
+                .returns(() => 'off')
+                .verifiable(TypeMoq.Times.atLeastOnce());
+
+            pythonConfigMock
+                .setup((c) => c.get('sortImports.args', []))
+                .returns(() => [])
+                .verifiable(TypeMoq.Times.atLeastOnce());
+            pythonConfigMock
+                .setup((c) => c.get('sortImports.path', ''))
+                .returns(() => 'isort')
+                .verifiable(TypeMoq.Times.atLeastOnce());
+
             const settings: ISettings = await getWorkspaceSettings('isort', workspace1);
             assert.deepStrictEqual(settings.args, []);
             assert.deepStrictEqual(settings.importStrategy, 'useBundled');
@@ -84,6 +89,9 @@ suite('Settings Tests', () => {
             assert.deepStrictEqual(settings.severity, {});
             assert.deepStrictEqual(settings.showNotifications, 'off');
             assert.deepStrictEqual(settings.workspace, workspace1.uri.toString());
+
+            configMock.verifyAll();
+            pythonConfigMock.verifyAll();
         });
     });
 });
