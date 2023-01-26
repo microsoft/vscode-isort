3f477d06bfb5fb698b26d93abeaff36ce038d3a9
Karthik Nadig <kanadig@microsoft.com>
2023-01-26 13:38:22 -0800

    Ensure serverless mode works with path set. (#210)

--- bundled/tool/script_runner.py
+++ bundled/tool/script_runner.py
@@ -1,7 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 """Implementation of tool support from command line."""
-from __future__ import annotations
 
 import os
 import pathlib
