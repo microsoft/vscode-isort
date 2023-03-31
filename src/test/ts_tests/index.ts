7fb037019c33dea0393e5e4841d7042542f9563c
Karthik Nadig <kanadig@microsoft.com>
2023-03-31 15:23:14 -0700

    Update all dependencies (#261)
    * Update all dependencies
    
    * Pin fs-extra
    
    * More build fixes

--- src/test/ts_tests/index.ts
+++ src/test/ts_tests/index.ts
@@ -1,6 +1,6 @@
-import * as path from 'path';
-import * as Mocha from 'mocha';
 import * as glob from 'glob';
+import Mocha from 'mocha';
+import * as path from 'path';
 
 export function run(): Promise<void> {
     // Create the mocha test
@@ -12,27 +12,23 @@ export function run(): Promise<void> {
     const testsRoot = path.resolve(__dirname, './tests');
 
     return new Promise((c, e) => {
-        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-            if (err) {
-                return e(err);
-            }
+        const files = glob.globSync('**/**.test.js', { cwd: testsRoot });
 
-            // Add files to the test suite
-            files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+        // Add files to the test suite
+        files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-            try {
-                // Run the mocha test
-                mocha.run((failures) => {
-                    if (failures > 0) {
-                        e(new Error(`${failures} tests failed.`));
-                    } else {
-                        c();
-                    }
-                });
-            } catch (err) {
-                console.error(err);
-                e(err);
-            }
-        });
+        try {
+            // Run the mocha test
+            mocha.run((failures) => {
+                if (failures > 0) {
+                    e(new Error(`${failures} tests failed.`));
+                } else {
+                    c();
+                }
+            });
+        } catch (err) {
+            console.error(err);
+            e(err);
+        }
     });
 }
