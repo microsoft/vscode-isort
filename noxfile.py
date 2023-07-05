62733e25457f8ed90740f882ab3828ff83b4e88a
Karthik Nadig <kanadig@microsoft.com>
2023-07-05 13:37:41 -0700

    Update version and packages for release

--- noxfile.py
+++ noxfile.py
@@ -13,10 +13,17 @@ import nox
 
 
 def _update_pip_packages(session: nox.Session) -> None:
-    session.run("pip-compile", "--generate-hashes", "--upgrade", "./requirements.in")
     session.run(
         "pip-compile",
         "--generate-hashes",
+        "--resolver=backtracking",
+        "--upgrade",
+        "./requirements.in",
+    )
+    session.run(
+        "pip-compile",
+        "--generate-hashes",
+        "--resolver=backtracking",
         "--upgrade",
         "./src/test/python_tests/requirements.in",
     )
@@ -64,7 +71,7 @@ def _update_npm_packages(session: nox.Session) -> None:
         new_package_json += "\n"
     package_json_path.write_text(new_package_json, encoding="utf-8")
 
-    session.run("npm", "audit", "fix", external=True)
+    session.run("npm", "audit", "fix", external=True, success_codes=[0, 1])
     session.run("npm", "install", external=True)
 
 
